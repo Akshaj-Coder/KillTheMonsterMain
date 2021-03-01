80f4e9182f38f042ddaa8cac02ad8f2a11a5c867
Akshaj-Coder <73394608+Akshaj-Coder@users.noreply.github.com>
2021-03-01 18:47:46 +0530

    Add files via upload

---
+++ Enemy.js
@@ -0,0 +1,25 @@
+class Enemy{
+    constructor(x,y,width,height){
+        var options = {
+            isStatic: false,
+        }
+
+        this.body = Bodies.rectangle(x,y,width,height,options);
+        this.width = width;
+        this.height = height;
+        World.add(world,this.body);
+    }
+
+    display() {
+        var pos = this.body.position;
+        var angle = this.body.angle;
+
+        push();
+        translate(pos.x,pos.y);
+        rotate(angle);
+        //rectMode(CENTER);
+        imageMode(CENTER);
+        image(enemyimg,0,0,this.width,this.height);
+        pop();
+    }
+}
